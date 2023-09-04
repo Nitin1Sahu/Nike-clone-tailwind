c8488fa21c6abac840e9d253bf94a42b0a7869e2
Nitin Sahu <sahunitin970@gmail.com>
2023-09-04 16:55:33 +0530

    Nike Clone Hero page

---
+++ src/components/Button.jsx
@@ -0,0 +1,10 @@
+const Button = ({ label, iconURL }) => {
+    return (
+        <button className="flex justify-center items-center gap-2 padding-x py-4 border font-montserrat text-xl leading-none bg-coral-red rounded-full text-white">
+            {label}
+            <img src={iconURL} alt="arrow right icon" className="ml-2 rounded-full w-5 h-5" />
+        </button>
+    )
+}
+
+export default Button
